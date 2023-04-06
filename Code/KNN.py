75e4717613211e1506c6487fd2a78a2abc396267
Wenhua-Jiang <101352550+Wenhua-Jiang@users.noreply.github.com>
2023-04-06 18:21:04 +0100

    Update KNN.py

--- Code/KNN.py
+++ Code/KNN.py
@@ -1,1 +1,8 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
